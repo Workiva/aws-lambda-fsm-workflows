b9b7c1922e6097ed9b1d89fa24b22108e31d1f16
Shawn Rusaw <shawn.rusaw@workiva.com>
2017-11-19 13:37:59 -0700

    fixing the links on the PyPI index page

--- aws_lambda_fsm/_pkg_meta.py
+++ aws_lambda_fsm/_pkg_meta.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (0, 9, 15)
+version_info = (0, 9, 16)
 version = '.'.join(map(str, version_info))
