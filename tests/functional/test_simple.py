b5a047c334d3ef0df11b83e0d8844594741e8dd5
Shawn Rusaw <shawn.rusaw@workiva.com>
2017-11-21 04:23:48 -0700

    added functional tests for mertrics

--- tests/functional/test_simple.py
+++ tests/functional/test_simple.py
@@ -42,11 +42,14 @@ class Messages(object):
         del self.messages[:]
         del self.all_messages[:]
 
-    def trace(self, uvars=()):
+    def trace(self, uvars=(), raw=False):
         s = 'system_context'
         u = 'user_context'
         svars = ('current_state', 'current_event', 'steps', 'retries')
-        data = enumerate(map(lambda x: json.loads(x), self.all_messages))
+        serialized = map(lambda x: json.loads(x), self.all_messages)
+        if raw:
+            return serialized
+        data = enumerate(serialized)
         return map(
             lambda x: (
                 x[0],
@@ -74,6 +77,10 @@ class AWSStub(object):
         self.primary_cache = {}
         self.secondary_cache = {}
 
+        self.all_caches = {}
+
+        self.errors = Messages()
+
     def _get_stream_source(self, primary):
         if self.primary_stream_source_failure and primary:
             raise ClientError({"Error": {}}, "mock")
@@ -89,6 +96,8 @@ class AWSStub(object):
         }[primary]
 
     def _get_retry_source(self, primary):
+        if self.primary_retry_source_failure and primary:
+            raise ClientError({"Error": {}}, "mock")
         return {
             True: self.primary_retry_source,
             False: self.secondary_retry_source
@@ -106,7 +115,9 @@ class AWSStub(object):
         return {'test': 'stub'}
 
     def set_message_dispatched(self, correlation_id, steps, retries, primary=True):
-        self._get_cache_source(primary)['%s-%s' % (correlation_id, steps)] = True
+        key = '%s-%s' % (correlation_id, steps)
+        self._get_cache_source(primary)[key] = True
+        self.all_caches[key] = True
         return True
 
     def get_message_dispatched(self, correlation_id, steps, primary=True):
@@ -118,6 +129,7 @@ class AWSStub(object):
             return False
         else:
             self._get_cache_source(primary)[key] = True
+            self.all_caches[key] = True
             return True
 
     def release_lease(self, correlation_id, steps, retries, fence_token, primary=True):
@@ -134,6 +146,7 @@ class AWSStub(object):
         return {'test': 'stub'}
 
     def increment_error_counters(self, data, dimensions):
+        self.errors.send(json.dumps((data, dimensions)))
         return {'test': 'stub'}
 
     def store_checkpoint(self, context, sent, primary=True):
@@ -147,7 +160,10 @@ class AWSStub(object):
         self.all_sources.reset()
         self.primary_cache.clear()
         self.secondary_cache.clear()
+        self.all_caches.clear()
         self.primary_stream_source_failure = False
+        self.primary_retry_source_failure = False
+        self.errors.reset()
 
 
 def to_kinesis_message(data):
@@ -178,25 +194,54 @@ class Test(unittest.TestCase):
         AWS.reset()
         FSM(get_current_configuration('tests/functional/fsm.yaml'))
 
-    def _execute(self, machine_name, context, fail_primary=False):
-        start_state_machine(machine_name, context)
-        if fail_primary:
+    def _execute(self, machine_name, context, fail_primary_stream_stream=False, fail_primary_stream_retry=False):
+        start_state_machine(machine_name, context, correlation_id='correlation_id')
+        if fail_primary_stream_stream:
             AWS.primary_stream_source_failure = True
+        if fail_primary_stream_retry:
+            AWS.primary_retry_source_failure = True
         message = AWS.get_message()
         while message:
             handler.lambda_kinesis_handler(to_kinesis_message(message))
             message = AWS.get_message()
 
+    ################################################################################
+    # START: machine_name="simple"
+    ################################################################################
+
     def test_simple(self, *args):
         self._execute("simple", {})
+
+        # check messages
         expected = [
             (0, ('pseudo_init', 'pseudo_init', 0, 0), ()),
             (1, ('start', 'ok', 1, 0), ())
         ]
         self.assertEqual(expected, AWS.all_sources.trace())
 
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = []
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
     def test_simple_with_primary_failure(self, *args):
-        self._execute("simple", {}, fail_primary=True)
+        self._execute("simple", {}, fail_primary_stream_stream=True)
+
+        # check messages
         expected = [
             (0, ('pseudo_init', 'pseudo_init', 0, 0), ()),
             (1, ('start', 'ok', 1, 0), ())
@@ -211,17 +256,117 @@ class Test(unittest.TestCase):
         ]
         self.assertEqual(expected, AWS.secondary_stream_source.trace())
 
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = [
+            [{'error': 1}, {'current_state': 'pseudo_init', 'current_event': 'pseudo_init', 'machine_name': 'simple'}]
+        ]
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
     def test_simple_with_failure(self, *args):
         self._execute("simple", {'fail_at': [(0, 0)]})
+
+        # check messages
         expected = [
             (0, ('pseudo_init', 'pseudo_init', 0, 0), ()),
             (1, ('pseudo_init', 'pseudo_init', 0, 1), ()),  # retry
             (2, ('start', 'ok', 1, 0), ())
         ]
         self.assertEqual(expected, AWS.all_sources.trace())
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = [
+            [
+                {'retry': 1},
+                {'current_state': 'pseudo_init', 'current_event': 'pseudo_init', 'machine_name': 'simple'}
+            ]
+        ]
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
+    def test_simple_with_failure_with_primary_retry_failure(self, *args):
+        self._execute("simple", {'fail_at': [(0, 0)]}, fail_primary_stream_retry=True)
+
+        # check messages
+        expected = [
+            (0, ('pseudo_init', 'pseudo_init', 0, 0), ()),
+            (1, ('pseudo_init', 'pseudo_init', 0, 1), ()),  # retry
+            (2, ('start', 'ok', 1, 0), ())
+        ]
+        self.assertEqual(expected, AWS.all_sources.trace())
+        expected = []
+        self.assertEqual(expected, AWS.primary_retry_source.trace())
+        expected = [
+            (0, ('pseudo_init', 'pseudo_init', 0, 1), ())
+        ]
+        self.assertEqual(expected, AWS.secondary_retry_source.trace())
+
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = [
+            [
+                {'retry': 1, 'error': 1},
+                {'current_state': 'pseudo_init', 'current_event': 'pseudo_init', 'machine_name': 'simple'}
+            ]
+        ]
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
+
+    ################################################################################
+    # START: machine_name="looper"
+    ################################################################################
 
     def test_looper(self, *args):
         self._execute("looper", {"loops": 3})
+
+        # check messages
         expected = [
             (0, ('pseudo_init', 'pseudo_init', 0, 0), (None,)),
             (1, ('start', 'ok', 1, 0), (1,)),
@@ -230,8 +375,35 @@ class Test(unittest.TestCase):
         ]
         self.assertEqual(expected, AWS.all_sources.trace(('counter',)))
 
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+            'lease-correlation_id-2': True,
+            'lease-correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = []
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
     def test_looper_with_primary_failure(self, *args):
-        self._execute("looper", {"loops": 3}, fail_primary=True)
+        self._execute("looper", {"loops": 3}, fail_primary_stream_stream=True)
+
+        # check messages
         expected = [
             (0, ('pseudo_init', 'pseudo_init', 0, 0), (None,)),
             (1, ('start', 'ok', 1, 0), (1,)),
@@ -250,8 +422,45 @@ class Test(unittest.TestCase):
         ]
         self.assertEqual(expected, AWS.secondary_stream_source.trace(('counter',)))
 
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+            'lease-correlation_id-2': True,
+            'lease-correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = [
+            [
+                {'error': 1}, {'current_state': 'pseudo_init', 'current_event': 'pseudo_init', 'machine_name': 'looper'}
+            ],
+            [
+                {'error': 1}, {'current_state': 'start', 'current_event': 'ok', 'machine_name': 'looper'}
+            ],
+            [
+                {'error': 1}, {'current_state': 'start', 'current_event': 'ok', 'machine_name': 'looper'}
+            ]
+        ]
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
     def test_looper_with_failure(self, *args):
         self._execute("looper", {"loops": 3, 'fail_at': [(1, 0)]})
+
+        # check messages
         expected = [
             (0, ('pseudo_init', 'pseudo_init', 0, 0), (None,)),
             (1, ('start', 'ok', 1, 0), (1,)),
@@ -260,3 +469,82 @@ class Test(unittest.TestCase):
             (4, ('start', 'done', 3, 0), (3,))
         ]
         self.assertEqual(expected, AWS.all_sources.trace(('counter',)))
+
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+            'lease-correlation_id-2': True,
+            'lease-correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = [
+            [
+                {'retry': 1},
+                {'current_state': 'start', 'current_event': 'ok', 'machine_name': 'looper'}
+            ]
+        ]
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
+
+    def test_looper_with_failure_with_primary_retry_failure(self, *args):
+        self._execute("looper", {"loops": 3, 'fail_at': [(1, 0)]}, fail_primary_stream_retry=True)
+
+        # check messages
+        expected = [
+            (0, ('pseudo_init', 'pseudo_init', 0, 0), (None,)),
+            (1, ('start', 'ok', 1, 0), (1,)),
+            (2, ('start', 'ok', 1, 1), (1,)),  # retry
+            (3, ('start', 'ok', 2, 0), (2,)),
+            (4, ('start', 'done', 3, 0), (3,))
+        ]
+        self.assertEqual(expected, AWS.all_sources.trace(('counter',)))
+        expected = []
+        self.assertEqual(expected, AWS.primary_retry_source.trace(('counter',)))
+        expected = [
+            (0, ('start', 'ok', 1, 1), (1,))
+        ]
+        self.assertEqual(expected, AWS.secondary_retry_source.trace(('counter',)))
+
+        # check cache
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.primary_cache)
+        self.assertEqual(expected, AWS.secondary_cache)
+        expected = {
+            'correlation_id-0': True,
+            'correlation_id-1': True,
+            'correlation_id-2': True,
+            'correlation_id-3': True,
+            'lease-correlation_id-0': True,
+            'lease-correlation_id-1': True,
+            'lease-correlation_id-2': True,
+            'lease-correlation_id-3': True
+        }
+        self.assertEqual(expected, AWS.all_caches)
+
+        # check errors
+        expected = [
+            [
+                {'retry': 1, 'error': 1},
+                {'current_state': 'start', 'current_event': 'ok', 'machine_name': 'looper'}
+            ]
+        ]
+        self.assertEqual(expected, AWS.errors.trace(raw=True))
