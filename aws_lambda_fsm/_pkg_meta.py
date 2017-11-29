c74d3e7ebb52bfd3cb766138a099fb46d32f46ef
Shawn Rusaw <shawn.rusaw@workiva.com>
2017-11-29 12:52:10 -0700

    bumped version to 0.9.17

--- aws_lambda_fsm/_pkg_meta.py
+++ aws_lambda_fsm/_pkg_meta.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (0, 9, 16)
+version_info = (0, 9, 17)
 version = '.'.join(map(str, version_info))
