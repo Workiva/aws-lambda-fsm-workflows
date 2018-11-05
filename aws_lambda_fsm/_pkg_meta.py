9c991c74f2636da9c65ed9247280e8873c7a8876
Aaron Kavlie <aaron.kavlie@workiva.com>
2018-11-05 15:43:48 -0700

    Remove builtins reference in _pkg_meta.py

--- aws_lambda_fsm/_pkg_meta.py
+++ aws_lambda_fsm/_pkg_meta.py
@@ -12,9 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# system imports
-from builtins import map
-
-
 version_info = (0, 22, 0)
 version = '.'.join(map(str, version_info))
